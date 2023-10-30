9f0f7cb2466255872ecc04b83000738013e319ee
georges <georges.laerche@gmail.com>
2023-10-30 16:10:05 +0100

    initialisation du projet

---
+++ src/components/Form.vue
@@ -0,0 +1,67 @@
+<script>
+    import FormButton from './FormButton.vue'
+
+    import { useTasksStore } from '../stores/tasks';
+
+    import{ ref } from 'vue';
+
+    const tasksStore = useTasksStore();
+    //console.log(tasksStore);
+    //tasksStore.tasks.push({
+    //    id: 1,
+    //    name: 'description de la tache 1 ',
+    //    completed: false
+    //});
+    
+    const id = ref('')
+    const name = ref('')
+    const description = ref('')
+    const completed = ref('')
+
+    const handleSubmit = () =>{
+        tasksStore.tasks.push({
+            id: tasksStore.tasks.length + 1,
+            name: name.value,
+            description: description.value,
+            completed: false,
+        })
+        name.value = '';
+        description.value = '';
+    }
+</script>
+
+<template>
+    <form @submit.prevent="handleSubmit">
+        
+        <div class="mb-3">
+          <label for="name" class="form-label">Nom</label>
+          <input 
+          v-model="name"
+          type="text" 
+          name="name" 
+          class="form-control" 
+          id="name" 
+          placeholder="Nom du projet">
+        </div>
+
+        <div class="mb-3">
+          <label for="description" class="form-label">Description</label>
+          <textarea 
+          v-model="description"
+          name="description" 
+          class="form-control" 
+          id="description" 
+          rows="3" 
+          placeholder="Description du projet"></textarea>
+        </div>
+
+        <FormButton buttonText="Envoyer"/>
+
+      </form>
+</template>
+
+
+
+<style scoped>
+
+</style>
---
+++ src/components/FormButton.vue
@@ -0,0 +1,19 @@
+<template>
+        <div class="col-auto">
+          <button class="btn btn-primary mb-3">{{ props.buttonText }}</button>
+        </div>
+</template>
+
+<script>
+const props = defineProps ({
+    buttonText: {
+        type: String,
+        required: true,
+    },
+})
+    
+</script>
+
+<style scoped>
+
+</style>
